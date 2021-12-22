9a32e685b6468e95314fddf2b44fb778572f53b8
Akashkapase9988 <akashkapase9988@gmail.com>
2021-12-22 09:01:09 +0530

    [Akash added code for start condition]

--- Gambler-Workshop/GamblerStrat.cs
+++ Gambler-Workshop/GamblerStrat.cs
@@ -6,11 +6,10 @@ namespace Gambler_Workshop
 {
     class GamblerStrat
     {
-        public static void Start() 
-        {
+
            const  int GamblerStake = 100;
           const int gambleer_Bet_per_game = 1;
-        }
+        
     }
 
 }
