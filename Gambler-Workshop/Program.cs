5725bdf9e29c53a225eb5f501cc038ba141df406
Akashkapase9988 <akashkapase9988@gmail.com>
2021-12-22 08:48:24 +0530

    Add project files.

---
+++ Gambler-Workshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gambler_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
